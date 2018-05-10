95fe040c171a9cd53049e31f6f3028de9c2a4334
sharonz2006 <xiurongzhu@gmail.com>
2018-05-10 11:07:05 -0500

    comments
    My first change

--- main_test.go
+++ main_test.go
@@ -14,7 +14,7 @@ func TestHandler(t *testing.T) {
 		Headers: map[string]string{
 			"Content-Type": "text/html",
 		},
-		Body: "Congratulations",
+		Body: "Congratulations, Sharon, you have successfullly run a simple go program",
 	}
 
 	response, err := Handler(request)
